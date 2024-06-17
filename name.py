10a9eae92871cc074ad81ffe0a7b5749e345f06b
GiftChrisJava <chrisjava77@gmail.com>
2024-06-17 11:28:03 +0200

    ugmented assignment

--- name.py
+++ name.py
@@ -4,14 +4,26 @@ print("Gift Chris")
 
 # print("His name is " + name)
 
-print(2 / 4)
-print(type(2 /4))
-print(type(2.0 / 4))
+# print(2 / 4)
+# print(type(2 /4))
+# print(type(2.0 / 4))
 
-print(2 **2) #power
-print(5 // 2)
-print(5 / 2)
-print(5 % 2)
+# print(2 **2) #power
+# print(5 // 2)
+# print(5 / 2)
+# print(5 % 2)
 
 
 #math functions
+print(round(3.16666666666, 3))
+print(abs(-20))
+
+a,b,c = 1,2,3
+
+print(a, b, c)
+
+
+#augmented assignment operators
+some_value = 5
+some_value += 2 #augmented assignment
+print(some_value)
