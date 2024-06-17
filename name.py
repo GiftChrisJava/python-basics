e453c40e4f23b9263f5ee79904243fe89366bfcf
GiftChrisJava <chrisjava77@gmail.com>
2024-06-17 10:57:43 +0200

    math functions

--- name.py
+++ name.py
@@ -1,5 +1,17 @@
 print("Gift Chris")
 
-name = input("what is your name ? ")
+# name = input("what is your name ? ")
 
-print("His name is " + name)
+# print("His name is " + name)
+
+print(2 / 4)
+print(type(2 /4))
+print(type(2.0 / 4))
+
+print(2 **2) #power
+print(5 // 2)
+print(5 / 2)
+print(5 % 2)
+
+
+#math functions
