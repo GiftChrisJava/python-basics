32e0a74ec3944b765a07033e94ab334424e19e5d
GiftChrisJava <chrisjava77@gmail.com>
2024-06-29 23:03:51 +0200

    modules

---
+++ functions/TestGCDFunction.py
@@ -0,0 +1,7 @@
+from GCDFunction import gcd
+
+# ask user to enter integers
+n1 = eval(input("Enter the first integer: "))
+n2 = eval(input("Enter the first integer: "))
+
+print("The greatest common divisor for", n1,"and", n2, "is", gcd(n1, n2))
